6b552a8619bd5ec67fc214eaaae6b00564c08b36
Muhamad Irgi Fahrezi <fahrezimuhamad28@gmail.com>
2021-09-25 11:26:01 +0700

    update add, detail

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './../services/crud.service';
 import { Router } from '@angular/router';
+import { ToastService } from './../services/toast.service';
 
 @Component({
   selector: 'app-employee-add',
@@ -8,17 +9,54 @@ import { Router } from '@angular/router';
   styleUrls: ['./employee-add.component.scss'],
 })
 export class EmployeeAddComponent implements OnInit {
-  constructor(private service: CrudService, private router: Router) {}
+  constructor(
+    private service: CrudService,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit(): void {}
 
   create(data) {
-    this.service.create(data.value).subscribe(() => {
-      this.router.navigate(['/list']);
-    });
+    if (
+      data.value.username &&
+      data.value.firstName &&
+      data.value.lastName &&
+      data.value.email &&
+      data.value.description &&
+      data.value.basicSalary &&
+      data.value.group &&
+      data.value.birthDate &&
+      (data.value.status == 0 || data.value.status == 1)
+    ) {
+      this.service.create(data.value).subscribe(() => {
+        this.router.navigate(['/list']);
+      });
+      this.showSuccess('New data succesfully added');
+    } else {
+      this.showError('All input must be filled');
+    }
   }
 
   cancel() {
     this.router.navigate(['/list']);
+  }
+
+  showSuccess(text) {
+    this.toastService.show(text, {
+      classname: 'bg-success text-light',
+      delay: 2000,
+      autohide: true,
+      headertext: 'Notifications',
+    });
+  }
+
+  showError(text) {
+    this.toastService.show(text, {
+      classname: 'bg-danger text-light',
+      delay: 2000,
+      autohide: true,
+      headertext: 'Notifications',
+    });
   }
 }
